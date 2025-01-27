d97a4b8a3e9e69b1f100a467d688737086e1fc1c
mohanson <mohanson@outlook.com>
2025-01-27 14:11:17 +0800

    2025-01-27 14:11:17

---
+++ pretty/cmd/table/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/mohanson/godump/pretty"
+)
+
+func main() {
+	pretty.PrintTable([][]string{
+		{"Coin", "Usable", "Freeze"},
+		{"bnb", "3.4247", "15.1042"},
+		{"btc", "0.0356", "0.2018"},
+		{"ckb", "526333.5353", "3160364.7891"},
+		{"eth", "0.4864", "2.5437"},
+		{"trx", "7498.283690", "33516.101561"},
+		{"usdt", "19494.875228", "71498.150403"},
+	})
+}
