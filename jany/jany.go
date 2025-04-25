99cbb7066859f95e0cb1847eead1656213cd9e83
mohanson <mohanson@outlook.com>
2025-04-25 11:26:11 +0800

    2025-04-25 11:26:11

--- jany/jany.go
+++ jany/jany.go
@@ -4,7 +4,6 @@ package jany
 import (
 	"encoding/json"
 	"io"
-	"iter"
 	"strconv"
 
 	"github.com/mohanson/godump/doa"
@@ -51,12 +50,14 @@ func (j *Jany) Float64() float64 {
 
 // Get returns the Jany[k].
 func (j *Jany) Get(k string) *Jany {
-	return j.Dict()[k]
+	a := j.j.(map[string]any)
+	return &Jany{j: a[k]}
 }
 
 // Idx returns the Jany[i].
 func (j *Jany) Idx(k int) *Jany {
-	return j.List()[k]
+	a := j.j.([]any)
+	return &Jany{j: a[k]}
 }
 
 // Int returns the int representation of the current Jany instance.
@@ -66,24 +67,12 @@ func (j *Jany) Int() int {
 
 // Int32 returns the int32 representation of the current Jany instance.
 func (j *Jany) Int32() int32 {
-	return int32(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 64)))
+	return int32(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 32)))
 }
 
 // Int64 returns the int64 representation of the current Jany instance.
 func (j *Jany) Int64() int64 {
 	return int64(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 64)))
-}
-
-// Iter returns a sequence from the current Jany instance's list.
-func (j *Jany) Iter() iter.Seq2[int, *Jany] {
-	a := j.j.([]any)
-	return func(yield func(int, *Jany) bool) {
-		for i, e := range a {
-			if !yield(i, &Jany{j: e}) {
-				return
-			}
-		}
-	}
 }
 
 // List returns the list representation of the current Jany instance.
@@ -99,6 +88,11 @@ func (j *Jany) List() []*Jany {
 // String returns the string representation of the current Jany instance.
 func (j *Jany) String() string {
 	return j.j.(string)
+}
+
+// Uint returns the uint representation of the current Jany instance.
+func (j *Jany) Uint() uint {
+	return uint(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 64)))
 }
 
 // Uint32 returns the uint32 representation of the current Jany instance.
