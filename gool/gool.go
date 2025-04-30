f6d5e94c03794b7dd97927b374fcdcbdc90bf5c6
mohanson <mohanson@outlook.com>
2025-04-30 12:45:19 +0800

    2025-04-30 12:45:19

--- gool/gool.go
+++ gool/gool.go
@@ -11,48 +11,48 @@ var cCpu = make(chan struct{}, runtime.NumCPU())
 
 // Gool manages a pool of goroutines for asynchronous task execution.
 type Gool struct {
-	C chan struct{}
-	M *sync.Mutex
-	W *sync.WaitGroup
+	c chan struct{}
+	m *sync.Mutex
+	w *sync.WaitGroup
 }
 
 // Call submits a function f for asynchronous execution in a new goroutine, respecting the concurrency limit.
 func (g *Gool) Call(f func()) {
-	g.C <- struct{}{}
-	g.W.Add(1)
+	g.c <- struct{}{}
+	g.w.Add(1)
 	go func() {
 		f()
-		g.W.Done()
-		<-g.C
+		g.w.Done()
+		<-g.c
 	}()
 }
 
 // Lock executes function f with exclusive access, synchronizing via the mutex, typically for aggregating results.
 func (g *Gool) Lock(f func()) {
-	g.M.Lock()
-	defer g.M.Unlock()
+	g.m.Lock()
+	defer g.m.Unlock()
 	f()
 }
 
 // Wait blocks until all submitted tasks have completed.
 func (g *Gool) Wait() {
-	g.W.Wait()
+	g.w.Wait()
 }
 
 // Cpu initializes a Gool instance with a global concurrency limit specified by cpu cores.
 func Cpu() *Gool {
 	return &Gool{
-		C: cCpu,
-		M: &sync.Mutex{},
-		W: &sync.WaitGroup{},
+		c: cCpu,
+		m: &sync.Mutex{},
+		w: &sync.WaitGroup{},
 	}
 }
 
 // New initializes a Gool instance with a custom concurrency limit specified by n.
 func New(n int) *Gool {
 	return &Gool{
-		C: make(chan struct{}, n),
-		M: &sync.Mutex{},
-		W: &sync.WaitGroup{},
+		c: make(chan struct{}, n),
+		m: &sync.Mutex{},
+		w: &sync.WaitGroup{},
 	}
 }
