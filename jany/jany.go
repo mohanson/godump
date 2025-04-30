7e0efeca9ac42e0f24140a79bb561634c137f0f7
mohanson <mohanson@outlook.com>
2025-04-30 16:20:15 +0800

    2025-04-30 16:20:15

--- jany/jany.go
+++ jany/jany.go
@@ -60,9 +60,14 @@ func (j *Jany) Idx(k int) *Jany {
 	return &Jany{j: a[k]}
 }
 
-// Int returns the int representation of the current Jany instance.
-func (j *Jany) Int() int {
-	return int(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 64)))
+// Int8 returns the int8 representation of the current Jany instance.
+func (j *Jany) Int8() int8 {
+	return int8(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 8)))
+}
+
+// Int16 returns the int16 representation of the current Jany instance.
+func (j *Jany) Int16() int16 {
+	return int16(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 16)))
 }
 
 // Int32 returns the int32 representation of the current Jany instance.
@@ -73,6 +78,11 @@ func (j *Jany) Int32() int32 {
 // Int64 returns the int64 representation of the current Jany instance.
 func (j *Jany) Int64() int64 {
 	return int64(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 64)))
+}
+
+// Int returns the int representation of the current Jany instance.
+func (j *Jany) Int() int {
+	return int(doa.Try(strconv.ParseInt(j.j.(json.Number).String(), 0, 64)))
 }
 
 // List returns the list representation of the current Jany instance.
@@ -90,9 +100,14 @@ func (j *Jany) String() string {
 	return j.j.(string)
 }
 
-// Uint returns the uint representation of the current Jany instance.
-func (j *Jany) Uint() uint {
-	return uint(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 64)))
+// Uint8 returns the uint8 representation of the current Jany instance.
+func (j *Jany) Uint8() uint8 {
+	return uint8(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 8)))
+}
+
+// Uint16 returns the uint16 representation of the current Jany instance.
+func (j *Jany) Uint16() uint16 {
+	return uint16(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 16)))
 }
 
 // Uint32 returns the uint32 representation of the current Jany instance.
@@ -103,4 +118,9 @@ func (j *Jany) Uint32() uint32 {
 // Uint64 returns the uint64 representation of the current Jany instance.
 func (j *Jany) Uint64() uint64 {
 	return uint64(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 64)))
+}
+
+// Uint returns the uint representation of the current Jany instance.
+func (j *Jany) Uint() uint {
+	return uint(doa.Try(strconv.ParseUint(j.j.(json.Number).String(), 0, 64)))
 }
