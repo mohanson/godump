784575e1c6b19ad6eb164f7c10407a05e98670cb
mohanson <mohanson@outlook.com>
2025-07-13 12:24:29 +0800

    2025-07-13 12:24:29

---
+++ rate/rate_test.go
@@ -0,0 +1,18 @@
+package rate
+
+import (
+	"math/rand/v2"
+	"testing"
+	"time"
+)
+
+func TestMain(t *testing.T) {
+	rate := NewLimits(128, time.Millisecond)
+	for range 1024 {
+		if rand.Uint32()%8 == 0 {
+			time.Sleep(time.Millisecond)
+		}
+		maxn := rate.capacity * 4
+		rate.Wait(rand.Uint64() % maxn)
+	}
+}
